8a69f79044e86073e394c219966cb0cfa28ccdda
sheetal <sheetalkumarilife@gmail.com>
2024-08-25 21:18:13 +0530

    working on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { auth } from "./firebase";
+import bottom from "./assets/bottom.svg"
 
 import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
@@ -13,9 +14,51 @@ export default function Home() {
   //     navigate("/");
   //   }
   // }, []);
+
+  const data =[
+    {
+      type:"FICTION",
+      subject:"fiction",
+    },{
+      type:"NON FICTION",
+      subject:"nonfiction",
+    },{
+      type:"HISTORICAL",
+      subject:"historical",
+    },{
+      type:"TRAVEL",
+      subject:"travel",
+    },{
+      type:"BUSINESS ECONOMICS",
+      subject:"business economics",
+    }
+  ]
   return (
     <div>
       <LoginNavbar/>
+      <div className="bg-hero bg-cover h-[85vh] w-[100%]  bg-[center_bottom_3rem] relative">
+          <div className="pt-16 text-center  text-white">
+
+        <h1 className=" text-7xl font-extrabold"
+        style={{   WebkitTextStroke: "1px black",
+          WebkitTextFillColor: "white",}}
+        >ReadWise</h1>
+        <p className="text-3xl text-white font-bold mt-5"
+                style={{   WebkitTextStroke: "1px black",
+                  WebkitTextFillColor: "white",}}>Unlock New Worlds, One Page at a Time.
+        Explore Books Now!</p>
+          </div>
+      <div className=" absolute bottom-[-20px]">
+          <img src={bottom}></img>
+        </div>
+      </div>
+           
+
+      <div className="mt-5">
+            {data.map((e)=>{
+              return <h1>{e.type}</h1>
+            })}
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
 "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'hero' : "url('./assets/hero1.jpg')",
+      }
+    },
   },
   plugins: [],
 }
