a5a27d0d1f73d3e79f693c8e8d96bf64b71361ae
sheetal <sheetalkumarilife@gmail.com>
2024-08-27 14:20:02 +0530

    done with chatbot

---
+++ src/Components/Gemini.jsx
@@ -0,0 +1,134 @@
+import React, { useEffect, useState } from "react";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Grid, Hourglass, ProgressBar, Vortex } from "react-loader-spinner";
+import{motion} from "framer-motion"
+import { RxEnter } from "react-icons/rx";
+
+const API_KEY = "AIzaSyDujSd_KNt_3XqUWkO8hlO82TFtBJ3ScQo";
+const genAI = new GoogleGenerativeAI(API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+function Gemini() {
+  const [prompt, setPrompt] = useState("");
+  const [chat,setChat] = useState("");
+  const [data, setData] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function fetchDataFromGemini(prompt) {
+    try {
+      if (!prompt) {
+        const notify = () => toast.warning("Provide a prompt");
+        notify();
+        return;
+      }
+      setLoading(true);
+      const response = await model.generateContent(prompt);
+      const result = response.response;
+      const text = result.text();
+    //   console.log(text);
+      setData(text);
+      setChat(prompt)
+      setLoading(false);
+      setPrompt("");
+    } catch (err) {
+      setLoading(false);
+
+      console.log(err);
+    }
+  }
+  return (
+    <div className="">
+    
+      <ToastContainer />
+
+     
+      { 
+        <GeminiPrompt
+          data={data}
+          setData={setData}
+          prompt={prompt}
+          setPrompt={setPrompt}
+          chat={chat}
+          setChat={setChat}
+          fetchDataFromGemini={fetchDataFromGemini}
+          loading={loading}
+        />
+      }
+    </div>
+  );
+}
+
+function GeminiPrompt({
+  data,
+  prompt,
+  setPrompt,
+  chat,
+  setData,
+  fetchDataFromGemini,
+  loading,
+}) 
+
+{
+  return (
+    <div
+     style={{fontFamily:"cursive"}}
+    className="border-4 border-purple-800 rounded-3xl w-1/2 m-auto ">
+        <div className="flex justify-between mx-4 mt-3">
+        <p className="text-2xl p-3 font-semibold text-blue-950"><i>HOW CAN I HELP YOU?✨✨</i> </p>
+        <motion.button
+        whileTap={{scale:0.9}}
+        onClick={()=>{
+            setData("")
+        }}
+        >❌</motion.button>
+        </div>
+      <div className="flex justify-evenly items-center">
+      <textarea
+      className="text-wrap w-[85%] p-1 m-3 rounded-lg border-2 border-black "
+        type="text"
+        value={prompt}
+        onChange={(e) => {setPrompt(e.target.value)
+        }}
+      />
+      <motion.button
+      whileTap={{scale:0.9}}
+      className=" bg-emerald-700 text-white h-14  rounded-full cursor-pointer px-4"
+        onClick={() => {
+          fetchDataFromGemini(prompt);
+        }}
+      >
+      <RxEnter className="text-2xl" />
+      </motion.button>
+      </div>
+      {loading && <div className="flex justify-center items-center p-2">
+        <Hourglass
+  visible={true}
+  height="30"
+  width="30"
+  ariaLabel="hourglass-loading"
+  wrapperStyle={{}}
+  wrapperClass=""
+  colors={['#306cce', '#72a1ed']}
+  />
+
+        </div>}
+      {data &&
+       <div className=" p-3 m-2">
+       <div className="flex items-center">
+        <img  className="w-10 h-10 rounded-full" src="https://pics.craiyon.com/2023-09-29/cc9c956c1a674b78a0c7e4f11cef1190.webp"></img>
+       <p className="ml-3 font-semibold">{chat}</p> 
+        </div>
+        <div className="flex mt-4">
+        <img  className="w-10 h-10 rounded-full" src="https://i.poweredtemplates.com/p/sp/128326/sp_slide_h_1.jpg"></img>
+        <pre className="ml-3 overflow-y-scroll text-wrap max-h-96 ">{data}
+        </pre>
+        </div>
+       
+        </div>}
+    </div>
+  );
+}
+
+export default Gemini;
--- src/Pages/Chatbot.jsx
+++ src/Pages/Chatbot.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import LoginNavbar from '../Components/LoginNavbar'
+import React from "react";
+import LoginNavbar from "../Components/LoginNavbar";
+import Gemini from "../Components/Gemini";
 
 function Chatbot() {
   return (
     <>
-    <LoginNavbar/>
+      <LoginNavbar />
+      <div>
+        <div className="flex  justify-center items-center">
+          <h1 className="text-6xl font-bold m-6 text-violet-900">
+            Welcome to our book chatbot!
+          </h1>
+          <img src="https://media.tenor.com/ta8QTcTW3OwAAAAM/st-george-georgi.gif"></img>
+        </div>
+        <p className="text-2xl text-blue-800 text-wrap w-[50%] m-auto text-center">
+          Simply type in your interests or questions, and our chatbot will
+          provide personalized book recommendations, genre exploration, author
+          details, and more. With our chatbot, finding your next favorite book
+          is fast, convenient, and tailored just for you. Start chatting now to
+          embark on your literary adventure!
+        </p>
+      </div>
+      <div className="my-20">
+        <Gemini/>
+      </div>
     </>
-  )
+  );
 }
 
-export default Chatbot
+export default Chatbot;
